f7eb3d8e624c286b5aefc73fb6d4c16b69d7915b
Abhishek Mane <abhishekmane@outlook.in>
2018-01-04 14:20:41 +0530

    :heavy_check_mark:

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask, request, send_from_directory, render_template
 import jinja2
 
 # initialize flask app
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 # Jinja2 configurations
+app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 # app.jinja_loader = jinja2.ChoiceLoader([
 #     app.jinja_loader,
 #     jinja2.FileSystemLoader(['/flaskapp/userdata',
@@ -19,4 +20,36 @@ def send_js(path):
 # Sample routes
 @app.route("/")
 def dashboard():
-    return render_template('index.html')
+    return render_template('index.pug')
+
+@app.route("/charts.html")
+def charts():
+    return render_template('charts.pug')
+
+@app.route("/tables.html")
+def tables():
+    return render_template('tables.pug')
+
+@app.route("/navbar.html")
+def navbar():
+    return render_template('navbar.pug')
+
+@app.route("/cards.html")
+def cards():
+    return render_template('cards.pug')
+
+@app.route("/login.html")
+def login():
+    return render_template('login.pug')
+
+@app.route("/register.html")
+def register():
+    return render_template('register.pug')
+
+@app.route("/forgot-password.html")
+def forgot_password():
+    return render_template('forgot-password.pug')
+
+@app.route("/blank.html")
+def blank():
+    return render_template('blank.pug')
